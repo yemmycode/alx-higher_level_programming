57436efef0046f24eb6816ce686c241c3efef8bb
yemmycode <yemiajibade17@gmail.com>
2024-03-12 16:54:32 -0700

    Python if else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    if i == 99:
-        print(f"{i}")
+for i in range(100):
+    if i is 99:
+        print("{}".format(i))
     else:
-        print(f"{i:02d}, ", end='')
+        print("{:02d}, ".format(i), end='')
