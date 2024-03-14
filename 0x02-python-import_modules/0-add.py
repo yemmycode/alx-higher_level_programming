14f795ad4ea9a80d76461d58de43117136c8bb21
yemmycode <yemiajibade17@gmail.com>
2024-03-14 15:44:29 -0700

    Python import and modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
 if __name__ == "__main__":
+ from add_0 import add
     a = 1
     b = 2
 
-    from add_0 import add
-
     print(f"{a} + {b} = {add(a, b)}")
-
