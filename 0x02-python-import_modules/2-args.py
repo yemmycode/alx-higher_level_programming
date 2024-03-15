d694e372958249257b57f1164f002238d3009c1b
yemmycode <yemiajibade17@gmail.com>
2024-03-14 17:25:57 -0700

    Python ArgV

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
+
 import sys
+x = len(sys.argv) - 1
+if x < 1:
+    print("{} arguments.".format(x))
+elif x == 1:
+    print("{} argument:".format(x))
+else:
+    print("{} arguments:".format(x))
 
-def main():
-    num_args = len(sys.argv) - 1
-
-    print(f"argument(s): {num_args}")
-
-    plural_suffix = 's' if num_args != 1 else ''
-
-    separator = ':'
-
-    print(f"Argument{plural_suffix} {separator}")
-
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
-
-if __name__ == "__main__":
-    main()
+for i in range(x):
+    print("{}: {:s}".format(i + 1, sys.argv[i + 1]))
