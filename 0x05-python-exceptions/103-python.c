bf96e06c6931788c0c637e4e31b887bdf0695a06
yemmycode <yemiajibade17@gmail.com>
2024-04-02 07:42:31 -0700

    C functions that print some basic info about Python lists, Python bytes an Python float objects

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -7,18 +7,18 @@ void print_python_float(PyObject *p);
 
 void print_python_list(PyObject *p) {
     if (!PyList_Check(p)) {
-        printf("[ERROR] Invalid PyListObject\n");
+        printf("[ERROR] PyListObject is invalid\n");
         return;
     }
 
     printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+    printf("[*] Size of the Python List: %ld\n", PyList_Size(p));
     fflush(stdout);
 }
 
 void print_python_bytes(PyObject *p) {
     if (!PyBytes_Check(p)) {
-        printf("[ERROR] Invalid PyBytesObject\n");
+        printf("[ERROR] PyBytesObject is invalid\n");
         return;
     }
 
@@ -26,10 +26,10 @@ void print_python_bytes(PyObject *p) {
     const char *bytes = PyBytes_AsString(p);
 
     printf("[.] bytes object info\n");
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", bytes);
+    printf("  Size: %ld\n", size);
+    printf("  Trying string: %s\n", bytes);
 
-    printf("[*] first %ld bytes: ", (size < 10) ? size : 10);
+    printf("[*] First %ld bytes: ", (size < 10) ? size : 10);
     for (Py_ssize_t i = 0; i < ((size < 10) ? size : 10); i++) {
         printf("%02hhx", bytes[i]);
         if (i < ((size < 10) ? size - 1 : 9)) {
@@ -42,13 +42,13 @@ void print_python_bytes(PyObject *p) {
 
 void print_python_float(PyObject *p) {
     if (!PyFloat_Check(p)) {
-        printf("[ERROR] Invalid PyFloatObject\n");
+        printf("[ERROR] PyFloatObject is invalid\n");
         return;
     }
 
     double value = PyFloat_AsDouble(p);
 
     printf("[.] float object info\n");
-    printf("  value: %.17g\n", value);
+    printf("  Value: %.17g\n", value);
     fflush(stdout);
 }
