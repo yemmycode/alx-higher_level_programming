e4517ea0d9adcfa20ed688273df1a46183a31486
yemmycode <yemiajibade17@gmail.com>
2024-04-09 15:43:28 -0700

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class MagicString:
-    def __init__(self):
-        self.n = 0
-
-    def magic_string(self):
-        self.n += 1
-        return "BestSchool, " * (self.n - 1) + "BestSchool"
+    def __init__(self): self.n = 0
+    def magic_string(self): self.n += 1; return "BestSchool, " * (self.n - 1) + "BestSchool"
