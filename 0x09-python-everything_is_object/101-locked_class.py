c8911f58156b0839aabaa656497dbe5ff53c00ef
yemmycode <yemiajibade17@gmail.com>
2024-04-09 16:20:40 -0700

    Everything is Object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ('first_name')
+    __slots__ = ('first_name',)
+    __setattr__ = lambda self, name, value: super().__setattr__(name, value) if hasattr(self, name) or name == 'first_name' else setattr(self, 'first_name', value)
+
