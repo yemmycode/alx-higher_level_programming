6a661a08f1d9bc05c9cbaac8904489370ed52e00
yemmycode <yemiajibade17@gmail.com>
2024-04-09 15:36:22 -0700

    function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def magic_string():
-    if not hasattr(magic_string, 'n'):
-        magic_string.n = 0
-    magic_string.n += 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+class MagicString:
+    def __init__(self):
+        self.n = 0
+
+    def magic_string(self):
+        self.n += 1
+        return "BestSchool, " * (self.n - 1) + "BestSchool"
