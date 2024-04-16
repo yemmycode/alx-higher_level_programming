2bce7b11ebb99954b26b1363aad1016637664faf
yemmycode <yemiajibade17@gmail.com>
2024-04-16 16:50:21 -0700

    task zero

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Function to read a UTF8 text file and output its contents to the console.
-"""
-
 
 def read_file(filename=""):
-    with open(filename, mode='r', encoding='utf-8') as file:
-        print(file.read(), end='')
+    """Reads the content of a text file (UTF8) and prints it to stdout."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
+
