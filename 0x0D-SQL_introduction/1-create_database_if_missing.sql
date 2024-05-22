afc4928fc390270fafd9c3d1ab52dbe97ba15463
yemmycode <yemiajibade17@gmail.com>
2024-05-21 20:06:34 -0700

    task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+#!/bin/bash
+
+MYSQL_USER="your_mysql_username"
+MYSQL_PASSWORD="your_mysql_password"
+
+CREATE_DB_QUERY="CREATE DATABASE IF NOT EXISTS hbtn_0c_0;"
+
+mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "$CREATE_DB_QUERY"
