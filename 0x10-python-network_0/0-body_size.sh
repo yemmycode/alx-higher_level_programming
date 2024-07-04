35d1c498b9b160f8a72113da19bbdd8dcd347093
yemmycode <yemiajibade17@gmail.com>
2024-07-04 04:30:47 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Retrieves and displays the size of the response body.
-# Usage: ./0-body_size.sh 0.0.0.0:50
+# Usage: ./script.sh <URL>
 
-curl_result=$(curl -sI "$1")
-content_length=$(echo "$curl_result" | grep "Content-Length:" | cut -d' ' -f 2)
-echo "$content_length"
+size=$(curl -s "$1" | wc -c)
+echo "$size"
